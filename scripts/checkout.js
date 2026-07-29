d1bb82a1797d2fe1ebdea34ba2a862dd776873ea
Shashwat Singh <shashwatsingh5381@gmail.com>
2026-07-29 23:12:08 +0530

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,22 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
 
+async function loadPage() {
+  await loadProducts();
+  
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+
+loadPage();
+
+/*
 loadProducts().then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
