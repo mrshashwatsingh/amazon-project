0b2df0d3046408629b5be36b09a6dee1a75648ff
Shashwat Singh <shashwatsingh5381@gmail.com>
2026-07-30 20:06:19 +0530

    Create order using the backend

---
+++ data/orders.js
@@ -0,0 +1,11 @@
+export let orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order) {
+  orders.unshift(order);
+  
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('orders', JSON.stringify(orders));
+}
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -62,7 +63,7 @@ export function renderPaymentSummary() {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
@@ -73,4 +74,26 @@ export function renderPaymentSummary() {
       ${totalQuantity} ${totalQuantity === 1 ? 'item' : 'items'}
     </a>)
   `;
+  
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart.cartItems
+        })
+      });
+      
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('Unexpected error try again later');
+    }
+    
+    
+    window.location.href = 'orders.html';
+  });
 }
